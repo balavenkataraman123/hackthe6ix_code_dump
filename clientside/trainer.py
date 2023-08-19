ecca7e62da384bc76032ac8f3bf58d79b775ed8f
Bala Venkataraman <balathegreat999@gmail.com>
2023-08-19 09:34:26 -0400

    e

--- clientside/trainer.py
+++ clientside/trainer.py
@@ -42,59 +42,80 @@ async def sockanal(websocket): # Analyzes websocket data
     global ipkeys
     global roomcrowds
 
-    remote_ip = websocket.remote_address[0] 
-
     async for fmessage in websocket:       
         userid = fmessage[:36] # gets header from websocket data
         message = fmessage[36:]
         if message[:100] == "data:,":
             await websocket.send("owo")
         else:
-            nparr = np.frombuffer(base64.b64decode(message.split(',')[1]), np.uint8) # decodes base64 image to Numpy matrix
-            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # converts matrix to opencv format image
+            try: 
+                thisface = uuidembeddings[userid][24]
+                await websocket.send("Facial data extraction complete")            
+            except:
 
-            image = cv2.resize(img, (640, 480))
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = face_mesh.process(image)
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            silhouette = [
-            10,  338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
-            397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
-            172, 58,  132, 93,  234, 127, 162, 21,  54,  103, 67,  109
-            ]
-            newimage = image
-            face = image
-            if results.multi_face_landmarks:
-                for face_landmarks in results.multi_face_landmarks:        
-                    xcoords = [handmark.x for handmark in face_landmarks.landmark]
-                    ycoords = [handmark.y for handmark in face_landmarks.landmark]
-                    sxcoords = [int(xcoords[i] * camw) for i in silhouette]
-                    sycoords = [int(ycoords[i] * camh) for i in silhouette]
-                    rex = int(xcoords[33] * camw)
-                    rey = int(ycoords[33] * camh)           
-                    lex = int(xcoords[263] * camw)
-                    ley = int(ycoords[263] * camh)
-                    midx = int((rex + lex)/2)
-                    midy = int((rey + ley)/2)
-                    angle = math.atan((rey-ley)/(rex-lex))
-                    deg = int(360 + (angle * (180/3.14159))) % 360
-                    print(deg)
-                    M = cv2.getRotationMatrix2D((midx, midy), deg, 1.0)
-                    print(M)
-                    sxx = [int(M[0][0] * sxcoords[i] + M[0][1] * sycoords[i] + M[0][2]) for i in range(len(sycoords))]
-                    sxy = [int(M[1][0] * sxcoords[i] + M[1][1] * sycoords[i] + M[1][2]) for i in range(len(sycoords))]
-                    newimage = cv2.warpAffine(image, M, (640, 480))            
-                    minx = min(sxx)
-                    maxx = max(sxx)
-                    miny = min(sxy)
-                    maxy = max(sxy)
-                    face = newimage[miny:maxy, minx:maxx]
-                    cv2.rectangle(newimage, (minx, miny), (maxx, maxy), (0,0,255), 3) 
-                    facevector = get_embedding(model, face)
-                    print(facevector)
+                nparr = np.frombuffer(base64.b64decode(message.split(',')[1]), np.uint8) # decodes base64 image to Numpy matrix
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # converts matrix to opencv format image
 
-            await websocket.send("image transfered successfully") # returns success message
+                image = cv2.resize(img, (640, 480))
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = face_mesh.process(image)
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                silhouette = [
+                10,  338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
+                397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
+                172, 58,  132, 93,  234, 127, 162, 21,  54,  103, 67,  109
+                ]
+                newimage = image
+                face = image
+                if results.multi_face_landmarks:
+                    for face_landmarks in results.multi_face_landmarks:        
+                        xcoords = [handmark.x for handmark in face_landmarks.landmark]
+                        ycoords = [handmark.y for handmark in face_landmarks.landmark]
+                        sxcoords = [int(xcoords[i] * camw) for i in silhouette]
+                        sycoords = [int(ycoords[i] * camh) for i in silhouette]
+                        rex = int(xcoords[33] * camw)
+                        rey = int(ycoords[33] * camh)           
+                        lex = int(xcoords[263] * camw)
+                        ley = int(ycoords[263] * camh)
+                        midx = int((rex + lex)/2)
+                        midy = int((rey + ley)/2)
+                        angle = math.atan((rey-ley)/(rex-lex))
+                        deg = int(360 + (angle * (180/3.14159))) % 360
+                        print(deg)
+                        M = cv2.getRotationMatrix2D((midx, midy), deg, 1.0)
+                        print(M)
+                        sxx = [int(M[0][0] * sxcoords[i] + M[0][1] * sycoords[i] + M[0][2]) for i in range(len(sycoords))]
+                        sxy = [int(M[1][0] * sxcoords[i] + M[1][1] * sycoords[i] + M[1][2]) for i in range(len(sycoords))]
+                        newimage = cv2.warpAffine(image, M, (640, 480))            
+                        minx = min(sxx)
+                        maxx = max(sxx)
+                        miny = min(sxy)
+                        maxy = max(sxy)
+                        face = newimage[miny:maxy, minx:maxx]
+                        cv2.rectangle(newimage, (minx, miny), (maxx, maxy), (0,0,255), 3) 
+                        facevector = get_embedding(model, face)
+                        print(facevector)
+                        try:
+                            thislist = uuidembeddings[]
+                        remote_ip = websocket.remote_address[0] 
+                        thislist = []
+                        try:
+                            thislist = uuidembeddings[userid]
+                        except:
+                            thislist = []
+                        toadd = True
+                        for i in thislist:
+                            if findCosineSimilarity(i, facevector) <= 0.2:
+                                toadd = False
+                        if toadd:
+                            thislist.append(facevector)
+                        if len(thislist) == 25:
+                            f = open(f'{userid}.csfd', 'wb')
+                            pickle.dump(faces, f)
+                            f.close()                        
+                                                                   
+                await websocket.send("Awaiting next face image") # returns success message
 
 
 async def main():
